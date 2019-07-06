1c41a7c29478e7022353a51674bf649ffe4bb13f
Adrian Eguez <eadepto@hotmail.com>
2019-07-06 09:47:33 -0500

    arania

--- 05_Scrapy/02_Crawling/python_csv/scrapy_03/scrapy_03/spiders/spider_item.py
+++ 05_Scrapy/02_Crawling/python_csv/scrapy_03/scrapy_03/spiders/spider_item.py
@@ -1,3 +1,45 @@
+import scrapy
+from scrapy_03.items import ProductoFybeca
+
+class AraniaProductosFybeca(scrapy.Spider):
+    name = 'arania_fybeca'
+
+    urls = [
+        'https://www.fybeca.com/FybecaWeb/pages/search-results.jsf?cat=238&s=0&pp=25'
+    ]
+
+    def parse_page(self, response):
+
+        productos = response.css('div.product-tile-inner')
+
+        for producto in productos:
+            existe_producto = producto.css('div.detail')
+
+            if(existe_producto.length > 0):
+                titulo = producto.css('a.name::text')
+                url = producto.xpath('/div[contains(@class,"detail")]/a[contains(@class,"image")]/img[contains(@id,"gImg")]/@src')
+                print(titulo.extract_first())
+                print(url.extract_first())
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 """
 
 url = response.xpath('//div[contains(@class,"detail")]/a[contains(@class,"image")]/img[contains(@id,"gImg")]/@src')
